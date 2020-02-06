97196c416779273d85b4fc1aa4d14f0aec44752b
MaxBlackCase <bladro7890@yandex.ru>
2020-02-06 17:47:51 +0400

    Закончет блок Resume

--- js/main.js
+++ js/main.js
@@ -397,19 +397,14 @@ $(".right-block__column").on("click", function(e) {
   e.preventDefault();
 
   var targetItem = $(this).attr("data-target");
-  
-  
-  console.log('targetItem: ', targetItem);
-  
-  
+
   $(".content").each(function(index, val) {
     $(".content__row").removeClass("active");
-    
-    if ($('.content__row').hasClass(targetItem)) {
 
+    if ($(".content__row").hasClass(targetItem)) {
       // * Content
       $(".close").addClass("active");
-      $("."+targetItem).addClass("active");
+      $("." + targetItem).addClass("active");
 
       // * Menu
       $(".left-column").addClass("active");
@@ -423,7 +418,7 @@ $(".close").on("click", function(e) {
   e.preventDefault();
 
   $(".content").each(function(index, val) {
-    $('.content__row').removeClass("active");
+    $(".content__row").removeClass("active");
   });
 
   $(".left-column").removeClass("active");
@@ -431,5 +426,17 @@ $(".close").on("click", function(e) {
   $(".close").removeClass("active");
   $(".wrapper__row").removeClass("active");
 });
+
+$(".chart").easyPieChart({
+  barColor: "#0080ff",
+  trackColor: "rgba(0, 128, 255, .2)",
+  scaleColor: "rgba(0, 128, 255, .6)",
+  scaleLength: 8,
+  animate: {
+    duration: 2000,
+    enabled: true
+  }
+});
+
 
 });
