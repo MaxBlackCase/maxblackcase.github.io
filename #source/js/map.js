724db55431b9203e52dee2a5d0342b341712df35
MaxBlackCase <bladro7890@yandex.ru>
2020-02-12 22:53:49 +0400

    update map.js

--- #source/js/map.js
+++ #source/js/map.js
@@ -10,15 +10,13 @@ function init() {
     center: [53.5088, 49.41918],
     zoom: 9
   });
-  myMap.behaviors.disable(["rightMouseButtonMagnifier"]);
-  myMap.behaviors.disable('scrollZoom');
+  myMap.behaviors.disable(["rightMouseButtonMagnifier",'scrollZoom','drag']);
   myMap.controls.remove("zoomControl");
   myMap.controls.remove("trafficControl");
   myMap.controls.remove("geolocationControl");
   myMap.controls.remove("typeSelector");
   myMap.controls.remove("fullscreenControl");
   myMap.controls.remove("rulerControl");
-  myMap.behaviors.disable('drag');
   var myMarker = new ymaps.Placemark(
     myMap.getCenter(),
     {
